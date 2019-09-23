1a8be6a13b69d8338bdb0dc2251d8abef0770185
Louis VI le Gros <39398756+LouisVIlegros@users.noreply.github.com>
2019-09-23 10:40:32 +0300

    change method names

--- Unit_Test/base_router_test.py
+++ Unit_Test/base_router_test.py
@@ -7,12 +7,12 @@ from base import BaseTestCase
 class FlaskTestCase(BaseTestCase):
 
     # Ensure that search by id works correctly 
-    def test_index(self):
+    def test_Get_By_Id(self):
         response = self.client.get('/search/5', content_type='application/json')
         self.assertEqual(response.status_code, 200)
 
     # Ensure that search TRIE works correctly : should return a list of users that name or last name begin by "ba"
-    def test_welcome_route_works_as_expected(self):
+    def test_TRIE_Search_works_as_expected(self):
         response = self.client.post('/search', data=dict(search : "ba", dataType : "users", actionType : "autocomplete"))
         self.assertIn(b'Welcome to Flask!', response.data)
 
